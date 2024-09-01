f2243ab1f43b9dbc7088475f16426b4f406a7613
Gabriel Blessing Christian Hutagaol <2381002@unai.edu>
2024-09-01 10:45:42 +0700

    dataType 1

---
+++ MembuatVariable.java
@@ -0,0 +1,20 @@
+public class MembuatVariable {
+    public static void main(String[] args) {
+        byte namaVariable;
+        byte nama_variable_satu; // python -> snakeCase
+        byte namaVariableSatu; // camelCase -> pakai yang ini
+
+        int showInfo = 10;
+        int bilanganBulat = 20;
+
+        String firstName = "Gabriel";
+        String lastName = "Hutagaol";
+
+        // Deklarasi, Inisilaisasi
+        String fullName;
+        fullName = "Gabriel Blessing Christian Hutagaol";
+
+        final double PHI_VALUE = 3.14;
+
+    }
+}
---
+++ TipeDataCharacter.java
@@ -0,0 +1,19 @@
+public class TipeDataCharacter {
+    public static void main(String[] args) {
+        char characterG = 'G';
+        char characterA = 'a';
+        char characterB = 'b';
+        char characterR = 'r';
+        char characterI = 'i';
+        char characterE = 'e';
+        char characterL = 'l';
+
+        System.out.println(characterG);
+        System.out.println(characterA);
+        System.out.println(characterB);
+        System.out.println(characterR);
+        System.out.println(characterI);
+        System.out.println(characterE);
+        System.out.println(characterL);
+    }
+}
